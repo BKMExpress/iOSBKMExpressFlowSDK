a95b4cbd7268783e9dff1b79da1c636293225c00
kguzel <kguzel@bkm.com.tr>
2017-10-24 10:45:18 +0300

    Initial Commit

---
+++ BKMExpressFlowSDKSample/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BKMExpressFlowSDKSample
+//
+//  Created by Kadir Guzel on 24/10/2017.
+//  Copyright © 2017 Kadir Guzel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
